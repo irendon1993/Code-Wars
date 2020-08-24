4aee9449efead21691f2b15cf6292deb7e12de7d
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2020-08-24 19:07:23 -0400

    Create SwitchItUp.cs

---
+++ SwitchItUp.cs
@@ -0,0 +1,10 @@
+public class Kata
+{
+  public static string SwitchItUp(int number)
+  {
+    string[] numbers =
+                { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" };
+
+    return numbers[number];
+  }
+}
